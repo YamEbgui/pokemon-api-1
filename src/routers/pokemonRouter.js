8ebc74db0d1ef2eca200df2a632e9be01ed8ecc5
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-10-21 16:51:45 +0300

    Get pokemon by ID & Name

--- src/routers/pokemonRouter.js
+++ src/routers/pokemonRouter.js
@@ -3,3 +3,41 @@ const router = express.Router();
 const Pokedex = require('pokedex-promise-v2');
 const P = new Pokedex();
 module.exports = router;
+
+router.get('/get/:id', async (request, response) => {
+	const { id } = request.params;
+	let pokemonObject = pokemonToRespond(await getPokemon(id));
+	response.send(pokemonObject);
+});
+
+router.get('/:query', async (request, response) => {
+	const { query } = request.params;
+	let pokemonObject = pokemonToRespond(await getPokemon(query));
+	response.send(pokemonObject);
+});
+
+router.put('/catch/:id', (request, response) => {});
+
+// function checkIfUserCatch(params) {}
+
+async function getPokemon(id) {
+	try {
+		return await P.getPokemonByName(id).then((response) => {
+			return response;
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function pokemonToRespond(pokemon) {
+	return {
+		name: pokemon.name,
+		height: pokemon.height,
+		weight: pokemon.weight,
+		types: pokemon.types,
+		front_pic: pokemon.sprites.front_default,
+		back_pic: pokemon.sprites.back_default,
+		abilities: pokemon.abilities,
+	};
+}
